3d7c01d42839ae15a5ba307dbf3bb656378545a3
Hammad Bawany <hammadcreativedesigne89@gmail.com>
2025-01-28 03:00:36 +0500

    Added carousel

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,6 @@
+import { url } from "inspector";
+import { title } from "process";
+
 const data = {
     headerMenus:[
         {
@@ -36,6 +39,35 @@ const data = {
         }
 
 
+    ],
+    carousels:[
+        {
+            title:"Most Popular Shoes For Sale",
+            buttonCaption:"Shop Now",
+            image:"/images/banner3.jpg",
+            url:"/search?category=Shoes",
+            isPublished:true,
+
+
+        },
+        {
+            title:"Best Sellers in T-shirts",
+            buttonCaption:"Shop Now",
+            image:"/images/banner1.jpg",
+            url:"/search?category=T-shirts",
+            isPublished:true,
+        },
+        {
+            title:"Best Deals on wrist watches",
+            buttonCaption:"See More", 
+            image:"/images/banner2.jpg",
+            url:"/search?category=Wrist Watches",
+            isPublished:true,
+
+        }
     ]
+
+
+
 }
 export default data;
