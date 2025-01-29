25c8c024bbc7beef487840fd473127ff05c6ed35
Hammad Bawany <hammadcreativedesigne89@gmail.com>
2025-01-29 22:53:38 +0500

    Solve NumSales prblm

--- lib/db/models/product.model.ts
+++ lib/db/models/product.model.ts
@@ -1,64 +1,98 @@
-import { IproductInput } from "@/types";
-import { Document, Schema, Model, model, models } from "mongoose";
+import { Document, Model, model, models, Schema } from 'mongoose'
+import { IProductInput } from '@/types'
 
-export interface Iproduct extends Document, IproductInput {
-    _id: string;
-    createdAt: Date;
-    updatedAt: Date;
+export interface IProduct extends Document, IProductInput {
+  _id: string
+  createdAt: Date
+  updatedAt: Date
 }
 
-const productSchema = new Schema<Iproduct>(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        slug: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        category: {
-            type: String,
-            required: true,
-        },
-        images: [String],
-        brand: {
-            type: String,
-            required: true,
-        },
-        description: { // Fixed typo here
-            type: String,
-            trim: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        listPrice: {
-            type: Number,
-            required: true,
-        },
-        countInStock: {
-            type: Number,
-            required: true,
-        },
-        tags: { type: [String], default: ['new arrival'] },
-        colors: { type: [String], default: ['White', 'Red', 'Black'] },
-        sizes: { type: [String], default: ['S', 'M', 'L', 'XL', 'XXL'] },
-        avgRating: { type: Number, default: 0, required: true },
-        numReviews: { type: Number, default: 0, required: true },
-        ratingDistribution: [{
-            rating: { type: Number, required: true },
-            count: { type: Number, required: true }
-        }],
-        numSales:{ type: Number, default: 0, required: true },
-        isPublished: { type: Boolean, default: false, required: true },
-        reviews: [{ type: Schema.Types.ObjectId, ref: 'Review', default: [] }],
+const productSchema = new Schema<IProduct>(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestamps: true } 
-);
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    images: [String],
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    listPrice: {
+      type: Number,
+      required: true,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+    },
+    tags: { type: [String], default: ['new arrival'] },
+    colors: { type: [String], default: ['White', 'Red', 'Black'] },
+    sizes: { type: [String], default: ['S', 'M', 'L'] },
+    avgRating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    ratingDistribution: [
+      {
+        rating: {
+          type: Number,
+          required: true,
+        },
+        count: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    numSales: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Review',
+        default: [],
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
 
-const Product = (models.Product as Model<Iproduct>) ||
-model<Iproduct>("Product", productSchema);
-export default Product;
+const Product =
+  (models.Product as Model<IProduct>) ||
+  model<IProduct>('Product', productSchema)
+
+export default Product
